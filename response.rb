2633d10a8b6c2e8602789e048b51c0b5db5f5d8f
Emmanuel Etti <emmanueletti@gmail.com>
2023-01-10 19:45:34 -0800

    Finishes rake app addition

--- response.rb
+++ response.rb
@@ -1,20 +1,30 @@
 class Response
-  def initialize(code:, body: "")
-    @code = code
-    @body = body
+  def initialize(status:, headers: {}, body: "")
+    @status = status
+    @headers = headers
+    @body = body.join
   end
 
   # can programmatically update content type depending on what body is being
   # sent
+
   def send(client)
-    client.print("HTTP/1.1 #{code}\r\n")
-    client.print("Content-Type: text/html\r\n")
+    client.print("HTTP/1.1 #{status}\r\n")
+    print_headers(client)
     client.print("\r\n")
     client.print("#{body}\r\n") unless body.nil?
 
-    puts "-> #{code}"
+    puts "-> #{status}"
     client.close
   end
 
-  attr_reader :code, :body
+  attr_reader :status, :headers, :body
+
+  private
+
+  def print_headers(client)
+    headers.each do |name, value|
+      client.print("#{name}: #{value}\r\n")
+    end
+  end
 end
